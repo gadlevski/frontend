9c232a24f34035deb2d6d2a5d1ba9bcf7170b445
andrei gadlevski <andrei@gadlevski>
2023-06-09 16:09:46 +0300

    02 Добавление архитектуры Feature-Sliced Design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
+import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 import {Suspense} from "react";
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import "./styles/index.scss";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
@@ -15,8 +15,8 @@ const App = () => {
       <Link to={"about"}>О нас</Link>
       <Suspense fallback={<div>Загрузка страницы</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
