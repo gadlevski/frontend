4c8069785635b6f01d31e52704a3521c1639e508
andrei gadlevski <andrei@gadlevski>
2023-06-09 17:11:24 +0300

    03 Добавление конфигурации для роутинга

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
 import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
+import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
-import {Suspense} from "react";
-import {Link, Route, Routes} from "react-router-dom";
+import {AppRouter} from "./providers/router";
 import "./styles/index.scss";
 
 const App = () => {
@@ -13,12 +11,7 @@ const App = () => {
       <button onClick={toggleTheme}>Переключить тему</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"about"}>О нас</Link>
-      <Suspense fallback={<div>Загрузка страницы</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
