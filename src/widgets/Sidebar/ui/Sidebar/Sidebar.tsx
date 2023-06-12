4f7a9c100f652d54d823f7595b11ee679d455779
andrei gadlevski <andrei@gadlevski>
2023-06-12 08:29:22 +0300

    06 Добавление Sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,28 @@
+import {FC, useState} from "react";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from "./Sidebar.module.scss";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar: FC<SidebarProps> = (props) => {
+  const {className, children} = props;
+  const [isCollapsed, setCollapsed] = useState<boolean>(false);
+  const onToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
+  return (
+    <div
+      className={classNames(cls.Sidebar, {[cls.collapsed]: isCollapsed}, [
+        className,
+      ])}
+    >
+      <button onClick={onToggle}>toggle</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+      </div>
+    </div>
+  );
+};
