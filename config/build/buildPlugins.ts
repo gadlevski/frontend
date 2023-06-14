c4cb46875b2846235c79d00b48703692c3536f74
andrei gadlevski <andrei@gadlevski>
2023-06-14 07:38:55 +0300

    07 Добавление мультиязычности с помощью i18n

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,17 +1,23 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
-import { BuildOptions } from "./types/config";
+import {BuildOptions} from "./types/config";
 
-export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({
+  paths,
+  isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HtmlWebpackPlugin({
-      template: paths.html
+      template: paths.html,
     }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash].css',
-      chunkFilename: 'css/[name].[contenthash].css',
-    })
+      filename: "css/[name].[contenthash].css",
+      chunkFilename: "css/[name].[contenthash].css",
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
   ];
 }
