bb66b21a93e6ae68f27897b5c0adb552cf89f3a5
andrei gadlevski <andrei@gadlevski>
2023-06-17 17:30:05 +0300

    12 Добавление лоадера и страницы 404

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,20 +1,23 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Загрузка страницы</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            element={<div className="page-wrapper">{element}</div>}
-            path={path}
-          />
-        ))}
-      </Routes>
-    </Suspense>
+    <Routes>
+      {Object.values(routeConfig).map(({ element, path }) => (
+        <Route
+          key={path}
+          element={
+            <Suspense fallback={<PageLoader />}>
+              <div className="page-wrapper">{element}</div>
+            </Suspense>
+          }
+          path={path}
+        />
+      ))}
+    </Routes>
   );
 };
 
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import './Loader.scss';
+
+
+export const Loader: FC = () => {
+  return (
+    <div className="lds-dual-ring"></div>
+  );
+};
