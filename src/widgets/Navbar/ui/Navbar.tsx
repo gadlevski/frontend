dc64d74c18da7238ea9178239a4fa7cbd619710b
andrei gadlevski <andrei@gadlevski>
2023-07-05 08:59:38 +0300

    22 Исправление. Добавление переводов для ссылок меню. Включение файла webpack.config.ts в проверку eslint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,12 +1,14 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation('link');
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
       <div className={cls.links}>
@@ -15,10 +17,10 @@ export const Navbar = ({ className }: NavbarProps) => {
           to={'/'}
           className={cls.mainLink}
         >
-          Главная
+          {t('main')}
         </AppLink>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'about'}>
-          О нас
+          {t('about us')}
         </AppLink>
       </div>
     </div>
