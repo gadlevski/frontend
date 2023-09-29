e0eb960aec8884bf2266dded8c05208229b061b4
andrei gadlevski <andrei@gadlevski>
2023-09-29 09:22:18 +0300

    66 Добавление переменной окружения apiUrl

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const api = axios.create({
   baseURL: __API__,
@@ -6,7 +7,7 @@ export const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('user');
+    const token = localStorage.getItem(USER_LOCALSTORAGE_KEY);
     if (token) {
       config.headers.authorization = token;
     }
