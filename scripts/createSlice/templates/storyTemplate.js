5f2c5fb18771c0451349700b9974dde0e0c64ea8
andrei gadlevski <andrei@gadlevski>
2023-10-03 13:51:54 +0300

    72 Скрипт для создания шаблонов (pages, features, entities)

---
+++ scripts/createSlice/templates/storyTemplate.js
@@ -0,0 +1,18 @@
+module.exports = (layer, componentName) => `import type { Meta, StoryObj } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { themeDecorator } from 'shared/config/storybook/themeDecorator';
+import { ${componentName} } from './${componentName}';
+
+const meta = {
+  title: '${layer}/${componentName}',
+  component: ${componentName},
+} satisfies Meta<typeof ${componentName}>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Normal: Story = {
+  args: {},
+  decorators: [themeDecorator(Theme.LIGHT)],
+};`;
