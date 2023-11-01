05f7b65ec18b3999cc37e4a440a655fc16683eda
andrei gadlevski <andrei@gadlevski>
2023-11-01 15:34:56 +0300

    111 е2е тестирование с Cypress. Тесты на роутер

--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,19 +1,31 @@
-// Need to use the React-specific entry point to import createApi
 import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import 'isomorphic-fetch';
 
 export const rtkApi = createApi({
+  // Указываем путь к редьюсеру в Redux Store
   reducerPath: 'api',
+
+  // Указываем базовый запрос
   baseQuery: fetchBaseQuery({
+    // Базовый URL для всех запросов
     baseUrl: __API__,
+
+    // Функция для подготовки заголовков перед отправкой запроса
     prepareHeaders: (headers) => {
+      // Получаем токен из LocalStorage
       const token = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+
+      // Если токен существует, добавляем его в заголовки
       if (token) {
         headers.set('Authorization', token);
       }
+
+      // Возвращаем подготовленные заголовки
       return headers;
     },
   }),
+
+  // Здесь будут указаны конечные точки (endpoints) API
   endpoints: () => ({}),
 });
--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,15 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import { Button, ThemeButton } from './Button';
 
-
+// Описание набора тестов для компонента "Кнопка"
 describe('Кнопка', () => {
+
+  // Тест для проверки, что кнопка содержит только текст
   test('Только текст внутри', () => {
+    // Рендеринг компонента Button с текстом 'TEST'
     render(<Button>TEST</Button>);
+
+    // Проверка, что элемент с текстом 'TEST' присутствует в документе
     expect(screen.getByText('TEST')).toBeInTheDocument();
   });
 
+  // Тест для проверки, что кнопка содержит текст и определенный класс
   test('Текст внутри и класс', () => {
+    // Рендеринг компонента Button с текстом 'TEST' и темой ThemeButton.CLEAR
     render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+
+    // Проверка, что элемент с текстом 'TEST' имеет класс 'clear'
     expect(screen.getByText('TEST')).toHaveClass('clear');
   });
 });
